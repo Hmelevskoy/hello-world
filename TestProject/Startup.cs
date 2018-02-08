5995cce75be2fdf9532a70ddbb09fb0e3e130e46
Dmitry Hmelevskoy <hmelevskoy@ukr.net>
2018-02-08 16:58:05 +0200

    Added comment to Startup.cs

--- TestProject/Startup.cs
+++ TestProject/Startup.cs
@@ -9,7 +9,7 @@ namespace TestProject
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-            //
+            //testing branch
         }
     }
 }
